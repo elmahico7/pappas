b0ba8a104c5e42c8d40151023301e17dfcff2f28
Harris <harris.kampourakis@mozaik.com>
2022-04-30 15:52:53 +0300

    template contact

--- wp-content/themes/webpack-gulp-wordpress-starter-theme/assets/src/js/scripts/counter.js
+++ wp-content/themes/webpack-gulp-wordpress-starter-theme/assets/src/js/scripts/counter.js
@@ -2,44 +2,45 @@
 let section = document.querySelector('.office__numbers');
 let started = false; 
 
-window.onscroll = function() {
-    if (window.scrollY >= section.offsetTop -800) {
-        if (!started) {
-            const getNumber = (counter) => {
-                return parseFloat(counter.dataset.countTo);
-            };
-            const getSpeed = (counter) => {
-                return parseFloat(counter.dataset.duration);
-            };
-            const updateTex = (counter, text) => {
-                counter.textContent = text;
-            };
-            const animate = (counter, countTo, duration) => {
-                let startTime = null;
-                let currentTime = Date.now();
-                const step = (currentTime) => {
-                    if (!startTime) {
-                        startTime = currentTime;
-                    }
-                    const progress = Math.min((currentTime - startTime) / duration, 1);
-                    const currentNum = Math.floor(progress * countTo);
-                    updateTex(counter, currentNum);
-                    if (progress < 1) {
-                        window.requestAnimationFrame(step);
-                    } else {
-                        window.cancelAnimationFrame(window.requestAnimationFrame(step));
-                    }
+if (section) {
+    window.onscroll = function() {
+        if (window.scrollY >= section.offsetTop -800) {
+            if (!started) {
+                const getNumber = (counter) => {
+                    return parseFloat(counter.dataset.countTo);
                 };
-                window.requestAnimationFrame(step);
-            };
-            const counters = document.querySelectorAll('.counter');
-            counters.forEach((counter) => {
-                const countTo = getNumber(counter);
-                const animationDuration = getSpeed(counter);
-                animate(counter, countTo, animationDuration);
-            });
+                const getSpeed = (counter) => {
+                    return parseFloat(counter.dataset.duration);
+                };
+                const updateTex = (counter, text) => {
+                    counter.textContent = text;
+                };
+                const animate = (counter, countTo, duration) => {
+                    let startTime = null;
+                    let currentTime = Date.now();
+                    const step = (currentTime) => {
+                        if (!startTime) {
+                            startTime = currentTime;
+                        }
+                        const progress = Math.min((currentTime - startTime) / duration, 1);
+                        const currentNum = Math.floor(progress * countTo);
+                        updateTex(counter, currentNum);
+                        if (progress < 1) {
+                            window.requestAnimationFrame(step);
+                        } else {
+                            window.cancelAnimationFrame(window.requestAnimationFrame(step));
+                        }
+                    };
+                    window.requestAnimationFrame(step);
+                };
+                const counters = document.querySelectorAll('.counter');
+                counters.forEach((counter) => {
+                    const countTo = getNumber(counter);
+                    const animationDuration = getSpeed(counter);
+                    animate(counter, countTo, animationDuration);
+                });
+            }
+            started = true;
         }
-        started = true;
-    }
-};
-
+    };
+}
